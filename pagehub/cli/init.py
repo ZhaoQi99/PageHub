b90e54f8f4d9b8cf367e2572f2dd816ae9683e7e
Qi Zhao <zhaoqi99@outlook.com>
2024-08-12 18:22:16 +0800

    feat: 🎸 init command support API_KEY

--- pagehub/cli/init.py
+++ pagehub/cli/init.py
@@ -1,5 +1,8 @@
+from datetime import timedelta
+
 import click
 from django.core.management import call_command
+from django.utils import timezone
 
 from pagehub import __version__
 
@@ -17,7 +20,19 @@ def init_command(*args, **kwargs):
     click.secho("\n{}".format("-" * 30), fg="green")
     click.secho("[√] Init successfully.", fg="green")
 
-    click.echo("""
+    from pagehub.authorization.models import APIToken
+
+    expired = timezone.now().astimezone() + timedelta(days=365 * 100)
+    token = APIToken.objects.create(expired=expired)
+    click.echo(
+        "Your API token: {token} (expired: {expired})".format(
+            token=click.style(token.token, fg="blue"),
+            expired=click.style(expired.strftime("%Y-%m-%d %H:%M:%S"), fg="red"),
+        )
+    )
+
+    click.echo(
+        """
 {hint} To start PageHub, run: 
     pagehub server # then visit http://127.0.0.1:8000
         """.format(
