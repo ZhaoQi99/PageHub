4cf740dbbd210e7cf451fdf0da8a9a2d0cdbb609
Qi Zhao <zhaoqi99@outlook.com>
2024-08-12 13:10:44 +0800

    feat: 🎸 APIToken Auth

--- pagehub/core/api.py
+++ pagehub/core/api.py
@@ -1,6 +1,19 @@
-from ninja import NinjaAPI
+from authorization.models import APIToken
+from ninja import NinjaAPI, errors
+from ninja.security import APIKeyHeader
 from record.api import router as record_router
 
-api = NinjaAPI()
+
+class APIKey(APIKeyHeader):
+    param_name = "Authorization"
+
+    def authenticate(self, request, key):
+        if token := APIToken.objects.filter(token=key).first():
+            if token.verify():
+                return token
+        raise errors.AuthenticationError
+
+
+api = NinjaAPI(auth=APIKey())
 
 api.add_router("/record/", record_router)
--- pagehub/core/settings.py
+++ pagehub/core/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "pagehub.record",
+    "pagehub.authorization",
 ]
 
 MIDDLEWARE = [
